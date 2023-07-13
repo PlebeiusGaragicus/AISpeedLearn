23d33a1976a66c1bfc02c97d7eb5b600154e5ce5
PlebbyG <plebeiusgaragicus@gmail.com>
2023-07-12 21:27:30 -0700

    add sveltekit

---
+++ speedlearn/src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-5">
+		<h1 class="h1">AI Speed Learn</h1>
+		<p>soon(tm)</p>
+
+		<div class="card p-4">
+			<input type="text" class="input p-4" placeholder="Question" />
+			<select class="select">
+				<option>Multiple Choice</option>
+				<option>True or False</option>
+			</select>
+			<radio class="radio">
+				<input type="radio" name="radio" value="1" />
+				<span>
+					<input type="text" class="input p-4" placeholder="Option" />
+				</span>
+			</radio>
+			<radio class="radio">
+				<input type="radio" name="radio" value="1" />
+				<span>
+					<input type="text" class="input p-4" placeholder="Option" />
+				</span>
+			</radio>
+			<!-- <input type="text" class="input p-4" placeholder="Option" /> -->
+			<button type="button" class="btn variant-filled">Add option</button>
+			<button type="button" class="btn variant-filled">Save question</button>
+		</div>
+	</div>
+</div>
